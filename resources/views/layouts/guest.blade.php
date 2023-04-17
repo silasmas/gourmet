b56790b5250b5b9db332192f53b532aa3c92ace9
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2023-04-17 18:28:49 +0100

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -41,20 +41,19 @@
         <!-- Custom CSS File -->
         <link href="{{ asset('assets/css/style.custom.css') }}" rel="stylesheet">
 
-        <title>Groupe Gourmet du Congo</title>
+        <title>Groupe Gourmet</title>
     </head>
 
     <body>
         <!-- ======= Header ======= -->
         <header id="header" class="header fixed-top d-flex align-items-center">
             <div class="container d-flex align-items-center justify-content-between">
-
-                <a href="index.html" class="logo d-flex align-items-center me-auto me-lg-0">
+                <a href="./" class="logo d-flex align-items-center me-auto me-lg-0">
                     <img src="{{ asset('assets/img/logo.png') }}" alt="">
-                    <h1>Groupe Gourmet</h1>
+                    <h1>Gourmet</h1>
                 </a>
 
-                <nav id="navbar" class="navbar">
+                <nav id="navbar" class="navbar shadow-0">
                     <ul>
                         <li><a href="#">Accueil</a></li>
                         <li><a href="#">Resto Traiteur</a></li>
@@ -74,6 +73,25 @@
                 <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
             </div>
         </header><!-- End Header -->
+
+        <!-- ======= Hero Section ======= -->
+        <section id="hero" class="hero d-flex align-items-center section-bg">
+    <div class="container">
+      <div class="row justify-content-between gy-5">
+        <div class="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center align-items-center align-items-lg-start text-center text-lg-start">
+          <h2 data-aos="fade-up">Enjoy Your Healthy<br>Delicious Food</h2>
+          <p data-aos="fade-up" data-aos-delay="100">Sed autem laudantium dolores. Voluptatem itaque ea consequatur eveniet. Eum quas beatae cumque eum quaerat.</p>
+          <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
+            <a href="#book-a-table" class="btn-book-a-table">Book a Table</a>
+            <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+          </div>
+        </div>
+        <div class="col-lg-5 order-1 order-lg-2 text-center text-lg-start">
+          <img src="assets/img/hero-img.png" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="300">
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero Section -->
 
 @yield('guest-content')
 
