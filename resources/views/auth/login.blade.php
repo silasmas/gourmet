bc079ffab15573cd8ff60f48f40bc9cd2825c598
silasmas <silasjmas@gmail.com>
2023-05-13 04:20:49 +0100

    ff

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
             <!-- Email Address -->
             <div>
                 <x-input-label for="email" :value="__('Email ou Téléphone')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                <x-text-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')"
                     required autofocus autocomplete="username" />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
