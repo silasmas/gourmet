0a02bb9bc4e7f8a874f82bb09a712972a77736e1
silasmas <silasjmas@gmail.com>
2023-05-14 21:27:24 +0100

    jfd

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/menu', [HomeController::class, 'menu'])->name('menu');
 Route::get('/kicoucou', [HomeController::class, 'kicoucou'])->name('kicoucou');
 Route::get('/atelier', [HomeController::class, 'atelier'])->name('atelier');
 Route::get('/sommelerie', [HomeController::class, 'sommelerie'])->name('sommelerie');
